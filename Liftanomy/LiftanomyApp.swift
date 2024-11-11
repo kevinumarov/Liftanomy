1a993bac359a4b09a533906c8aa4f6be00000447
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-11 20:02:26 +0900

    Initial Commit

---
+++ Liftanomy/LiftanomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftanomyApp.swift
+//  Liftanomy
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LiftanomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
